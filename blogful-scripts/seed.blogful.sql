58a8bee93612b7f2907c3e2a9c9b8f0622a5c407
Carlo Giustozzi <carlog1118@gmail.com>
2020-03-09 12:48:44 -0400

    added shopping-list tests

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Fish tricks', now() - '21 days'::INTERVAL, 'main'),
+    ('Not Dogs', now() - '20 days'::INTERVAL, 'snack'),
+    ('Bluffalo Wings', now() - '19 days'::INTERVAL, 'snack'),
+    ('SubstiTuna Salad', now() - '18 days'::INTERVAL, 'lunch'),
+    ('Tofurkey', now() - '17 days'::INTERVAL, 'breakfast'),
+    ('Pretenderloins', now() - '16 days'::INTERVAL, 'main'),
+    ('Steak-believe', now() - '15 days'::INTERVAL, 'main'),
+    ('Kale Seitan', now() - '14 days'::INTERVAL, 'breakfast'),
+    ('NoBull Burger', now() - '13 days'::INTERVAL, 'snack'),
+    ('Turnip the Beet', now() - '12 days'::INTERVAL, 'lunch'),
+    ('Mascarphony', now() - '11 days'::INTERVAL, 'lunch'),
+    ('Burgatory', now() - '10 days'::INTERVAL, 'main'),
+    ('Sleight of Ham', now() - '9 days'::INTERVAL, 'lunch'),
+    ('Antichovies', now() - '8 days'::INTERVAL, 'breakfast'),
+    ('Lettuce B. Frank', now() - '7 days'::INTERVAL, 'lunch'),
+    ('Pepperphony', now() - '6 days'::INTERVAL, 'breakfast'),
+    ('Shamburger', now() - '5 days'::INTERVAL, 'main'),
+    ('Facon', now() - '4 days'::INTERVAL, 'breakfast'),
+    ('Salami-get-this-straight', now() - '3 days'::INTERVAL, 'main'),
+    ('Mi-steak', now() - '2 days'::INTERVAL, 'main'),
+    ('Ghost Beef', now() - '1 days'::INTERVAL, 'main')
+;
---
+++ src/shopping-list-service.js
@@ -0,0 +1,28 @@
+const ShoppingListService = {
+    getAllItems(knex) {
+        return knex
+            .select('*')
+            .from('shopping_list');
+    },
+    getById(knex, id) {
+        return knex
+            .from('shopping_list')
+            .select('*')
+            .where('id', id)
+            .first();
+    },
+    deleteItem(knex, id) {
+        return knex('shopping_list')
+            .where({ id })
+            .delete();
+    },
+    insertItem(knex, newItem) {
+        return knex
+            .insert(newItem)
+            .into('shopping_list')
+            .returning('*')
+            .then(rows => rows[0]);
+    },
+};
+
+module.exports = ShoppingListService;
